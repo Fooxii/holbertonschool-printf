486fee9bf69ceade963335bc122ce8471e34f219
Fooxii <ironata07@gmail.com>
2025-11-19 17:27:09 -0400

    Base loop to go through format and checks if format is null

--- printf_project/_printf.c
+++ printf_project/_printf.c
@@ -13,4 +13,26 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
+
+if (format == (void *)0) /*Checks for null*/
+{
+return (-1); /*Returns -1 because a successful return should be positive*/
+}
+va_start(args, format);
+while (*format != "\0") /*goes through every char in format pointer*/
+{
+if (*format == '%') /*if '%' is found check next character*/
+{
+format++; /* moves format to after the '%'*/
+
+/*Check for character after '%*/
+
+}
+
+else
+{
+write(1, format, 1); /*else writes the character*/
+count++;
+}
+}
 }
