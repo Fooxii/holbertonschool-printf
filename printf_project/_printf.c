3f40d06cc2de135d2cfc435f82c208f8e6f1fb3b
Fooxii <ironata07@gmail.com>
2025-11-20 14:10:42 -0400

    Finished _printf.c file by adding action after '%' is found

--- printf_project/_printf.c
+++ printf_project/_printf.c
@@ -1,12 +1,11 @@
-#include <stdarg.h> /* To handle variable arguments (va_list, va_start, va_end) */
+#include <stdarg.h> /* To handle variable arguments*/
 #include <unistd.h> /* Required for write () function to print characters */
-#include "main.h" /* Provided: includes the function prototypes as instructed */
+#include "main.h" /* Provided: includes the function prototypes as instructed*/
 
-/*
+/**
  * _printf - produces output according to a format
- * Prototype provided in main.h
- * Takes a format string and variable arguments
- * Returns the number of characters printed (excluding null byte)
+ * @format: pointer to string
+ * Return: the number of characters printed (excluding null byte)
  */
 
 int _printf(const char *format, ...)
@@ -18,21 +17,33 @@ if (format == (void *)0) /*Checks for null*/
 {
 return (-1); /*Returns -1 because a successful return should be positive*/
 }
+
 va_start(args, format);
 while (*format != "\0") /*goes through every char in format pointer*/
 {
 if (*format == '%') /*if '%' is found check next character*/
 {
-format++; /* moves format to after the '%'*/    
+format++; /* moves format to after the '%'*/
 
-/*Check for character after '%*/
+if (*format != '\0')
+{
+/* picks correct specifier and adds return to the count*/
+count += pick_specifier(format, args);
+}
+else
+{
+return (-1); /*if '%' is the last character*/
+}
 
 }
 
 else
 {
 write(1, format, 1); /*else writes the character*/
-count++;
+count++; /* increases count per printed character*/
 }
+format++;
 }
+va_end(args);
+return (count);
 }
