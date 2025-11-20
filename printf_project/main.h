b97991d0543712b3f78e99e46ed847f13a0d558f
Fooxii <ironata07@gmail.com>
2025-11-20 13:43:25 -0400

    updated main.h

--- printf_project/main.h
+++ printf_project/main.h
@@ -5,5 +5,9 @@
  */
 
 int _printf(const char *format, ...); /* Provided prototype */ 
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
+int pick_specifier(const char *format, va_list args);
 
 #endif /* MAIN_H */
