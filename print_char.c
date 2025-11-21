1543eb40b7caf126914ca841f7cd065c0c6431c4
alexander <acrcpr@gmail.com>
2025-11-21 12:05:05 -0400

    updated char.c file

---
+++ print_char.c
@@ -0,0 +1,20 @@
+#include <stdarg.h> /* for va_list */
+#include <unistd.h> /* for write() */
+#include "main.h" /* prototype */
+
+/**
+ * print_char / prints one character
+ * @args: list of the arguments
+ * Return: the number of characters printed 1
+ */
+int print_char(va_list args)
+{
+    char c;
+
+    c = va_arg(args, int);
+
+    write(1, &c, 1);
+
+    return (1);
+}
+
