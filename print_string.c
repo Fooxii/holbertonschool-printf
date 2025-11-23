1d5ba3c13383a74b456e811570423fd04ac35c69
Fooxii <ironata07@gmail.com>
2025-11-23 12:43:00 -0400

    betty and typo fixes

--- print_string.c
+++ print_string.c
@@ -1,27 +1,25 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * print_string
+ * print_string - function that prints string
  * @args: list of arguments
  * Return: number of characters in which are printed
  */
 int print_string(va_list args)
 {
-    char *str;
-    int i = 0;
+char *str;
+int i = 0;
 
-    str = va_arg(args, char *); 
+str = va_arg(args, char *); 
 
-    if (str == (void *)0)   /* handles NULL string */
-        str = "(null)";
+if (str == (void *)0)   /* handles NULL string */
+str = "(null)";
 
-        while (str[i] ~!= '\0')
-        {
-            write(1, &str[i], 1); /* print char */
-            i++;
-        }
+while (str[i] != '\0')
+{
+write(1, &str[i], 1); /* print char */
+i++;
+}
         
-    return (i); /* number of printed characters */
+return (i); /* number of printed characters */
 }
