2edc3b08bfa3f3d4d2ac4dfe1cea5fe469a5a00a
Fooxii <ironata07@gmail.com>
2025-11-23 00:06:48 -0400

    betty fix for print_char.c

--- print_char.c
+++ print_char.c
@@ -9,12 +9,11 @@
  */
 int print_char(va_list args)
 {
-    char c;
+char c;
 
-    c = va_arg(args, int);
+c = va_arg(args, int);
 
-    write(1, &c, 1);
+write(1, &c, 1);
 
-    return (1);
+return (1);
 }
-
