dbc15b3dd56bef78b42221b6e0d0983eb85b47f8
Fooxii <ironata07@gmail.com>
2025-11-23 00:07:15 -0400

    Added script for percent specifier

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_percent - prints a percent sign
+ * Return: number of characters printed (always 1)
+ */
+int print_percent(void)
+{
+    write(1, "%", 1);
+    return (1);
+}
